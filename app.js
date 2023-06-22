14e3f29e40c11b71bd3e0772a0cb831f1e1eb4bc
Thanseer <tthanseer407@gmail.com>
2023-06-23 00:36:52 +0530

    added modified app.js and others

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
 
 const express = require("express");
 const bodyparser = require("body-parser");
+const ejs = require("ejs");
 
 
 const app = express();
+app.set("view engine", "ejs");
+
 
 app.get("/", function(req, res){
-    res.send("Testing");
+    res.render("login.ejs");
 });
+app.get("/home", function(req, res){
+    res.render("home.ejs");
+});
+
+app.post("/home", function(req, res){
+    const username = req.body.Username;
+    const password = req.body.password;
+
+    console.log("s")
+
+});
+
+
+
+
+
+
+
 
 
 app.listen(3000, function(req, res){
